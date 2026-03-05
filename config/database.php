d3c01c782c07d5040ac763462bf9c09441357840
ucgbgt <ucfootwear@gmail.com>
2026-03-06 01:08:56 +0700

    tambah

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'stridenation');
+define('DB_NAME', 'bayarqri_stridenation');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_CHARSET', 'utf8mb4');
